991c7672895a76c7834576ddadaf56b7aa4944a3
Jeremy <Lexdexic@gmail.com>
2018-02-21 19:04:13 -0700

    fixn' typo like a boss

--- data/extradata.js
+++ data/extradata.js
@@ -1,4 +1,4 @@
 // i just put this here so i could make a commit, i just added to it for the same reason made this a comment
 // this is my favorite file so far
 // not only did I add some more junk I also created a branch
-just an FYI we might eed o put actual code in here
+just an FYI we might need to put actual code  here
