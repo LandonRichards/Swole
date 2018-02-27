300dab5873f2b9ea4c4940bab97d6f1618818772
Travis Roemhild <travis.roemhild@gmail.com>
2018-02-26 19:58:45 -0700

    changed port

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
-
-
 var app = express();
 
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 // parse application/x-www-form-urlencoded
@@ -23,8 +21,8 @@ require("./routes/html-routes.js")(app);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: false }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+  app.listen(port, function() {
+    console.log("App listening on PORT " + port);
   });
 });
 // });
