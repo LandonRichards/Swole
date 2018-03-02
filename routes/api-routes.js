0f25555da7c97707a3ef605db2be66177cded0a7
Landon <landonchristopherrichards@gmail.com>
2018-03-01 22:28:50 -0700

    final??

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,9 @@
 var db = require("../models");
 
 module.exports = function(app) {
-    app.post("/api/user/", function(req, res) {
+
+    app.post("/api/user", function(req, res) {
+
         db.Users.create({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
@@ -14,42 +16,95 @@ module.exports = function(app) {
         });
     });
 
-    app.put("/api/workouts/", function(req, res) {
+
+    app.post("/api/messages", function(req,res){
+        db.MessageBoards.create({
+            userName:req.body.userName,
+            message:req.body.message
+        }).then(function(data){
+            res.json(data);
+        });
+    });
+
+    app.get("/api/messages", function(req, res) {
+        db.MessageBoards.findAll({limit:10})
+            .then(function(data) {
+                res.json(data)
+            }).catch(function(err){
+            console.log(err)
+            res.json(err)
+        });
+    });
+
+    app.get("/api/leaders", function(req, res) {
+        db.MessageBoards.findAll({
+            limit:5,
+            
+        })
+            .then(function(data) {
+                res.json(data)
+            }).catch(function(err){
+            console.log(err)
+            res.json(err)
+        });
+    });
+
+    app.get("/api/workouts/:userName", function(req,res){
+        db.Workouts.findAll({
+            limit:10,
+            order: [ [ 'createdAt', 'DESC' ]],
+            where: {
+                userName: req.params.userName
+            }
+        }).then(function(data){
+            res.json(data)
+        }).catch(function(err){
+            console.log(err)
+            res.json(err)
+        })
+    })
+
+    app.post("/api/workouts", function(req, res) {
+        db.Workouts.create({
+            userName: req.body.userName,
+            workoutTitle: req.body.workoutTitle,
+            points: req.body.points
+        }).then(function(data) {
+            res.json(data)
+        });
+    });
+
+    app.put("/api/users", function(req,res){
+        console.log(req.body.points,req.body.userName)
         db.Users.update({
-            points: points + req.body.points
+            points: points += req.body.points
+
         }, {
             where: {
                 userName: req.body.userName
             }
+
+        }).then(function(data){
+            res.json(data);
         });
-    });
+    })
 
-    // app.post("/api/workouts/", function(req, res) {
-    //     db.Workouts.create({
-    //         userName: ,
-    //         workoutTitle: ,
-    //         points: 
-    //     }).then(function(data) {
-    //         res.json(data)
-    //     });
-    // });
-
-    app.post("/api/users/", function(req, res) {
-        console.log(req.body)
+    app.post("/api/users", function(req, res) {
         db.Users.findOne({
             where: {
                 userName: req.body.userName,
-                passWord: req.body.passWord
+                password: req.body.password
             }
         }).then(function(user) {
             if (user) {
-                console.log('Found')
-                res.send({ userName: req.body.userName });
+                res.send(user.dataValues.userName)
+
             } else {
                 console.log('No user found')
             };
         })
     });
+
 
     app.get("/api/messages", function(req, res) {
         db.STMB.findAll({ limit: 10, order: '"createdAt" DESC' })
@@ -57,6 +112,7 @@ module.exports = function(app) {
                 res.json(data)
             })
     })
+
 
     
 }
